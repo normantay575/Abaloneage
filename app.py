6fb8d06c8ead165740247a93d65f54095e93e16c
normantay575 <108395337+normantay575@users.noreply.github.com>
2022-07-07 17:39:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,11 +58,23 @@ def predict():
                     
         }]}
 
-    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/c258bae8-1c4e-4341-9462-9f2548c369e1/predictions?version=2022-07-07', json=payload_scoring,
+import requests
+
+# NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
+API_KEY = "<your API key>"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":
+ API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
+# NOTE: manually define and pass the array(s) of values to be scored in the next line
+payload_scoring = {"input_data": [{"fields": [array_of_input_fields], "values": [array_of_values_to_be_scored, another_array_of_values_to_be_scored]}]}
+
+response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/c258bae8-1c4e-4341-9462-9f2548c369e1/predictions?version=2022-07-07', json=payload_scoring,
  headers={'Authorization': 'Bearer ' + mltoken})
-    print("Scoring response")
-    print(response_scoring.json())
-    ####################### END OF AUTOAI DEPLOYMENT API #######################
+print("Scoring response")
+print(response_scoring.json())
 
     return render_template('index.html', prediction_text='Abalone age is $ {}'.format(response_scoring.json()))
 
